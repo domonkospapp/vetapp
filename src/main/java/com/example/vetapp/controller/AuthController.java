4d4e27e8974e9d38c9563e24f06b8f321b49d76c
Domonkos Papp <pappdomi5@gmail.com>
2019-07-03 00:59:11 +0200

    Registration refact

--- src/main/java/com/example/vetapp/controller/AuthController.java
+++ src/main/java/com/example/vetapp/controller/AuthController.java
@@ -66,43 +66,16 @@ public class AuthController {
  
     @PostMapping("/signup")
     public ResponseEntity<String> registerUser(@Valid @RequestBody SignUpForm signUpRequest) {
-        if(userRepository.existsByUsername(signUpRequest.getUsername())) {
-            return new ResponseEntity<String>("Fail -> Username is already taken!",
-                    HttpStatus.BAD_REQUEST);
-        }
+        if(userRepository.existsByUsername(signUpRequest.getUsername()))
+            return new ResponseEntity<String>("Fail -> Username is already taken!", HttpStatus.BAD_REQUEST);
+        if(userRepository.existsByEmail(signUpRequest.getEmail()))
+            return new ResponseEntity<String>("Fail -> Email is already in use!", HttpStatus.BAD_REQUEST);
+        
+        User user = new User(signUpRequest.getName(), signUpRequest.getUsername(), signUpRequest.getEmail(), encoder.encode(signUpRequest.getPassword()));
  
-        if(userRepository.existsByEmail(signUpRequest.getEmail())) {
-            return new ResponseEntity<String>("Fail -> Email is already in use!",
-                    HttpStatus.BAD_REQUEST);
-        }
- 
-        // Creating user's account
-        User user = new User(signUpRequest.getName(), signUpRequest.getUsername(),
-                signUpRequest.getEmail(), encoder.encode(signUpRequest.getPassword()));
- 
-        Set<String> strRoles = signUpRequest.getRole();
+        Role userRole = roleRepository.findByName(RoleName.ROLE_USER).orElseThrow(() -> new RuntimeException("Fail! -> Cause: User Role not find."));
         Set<Role> roles = new HashSet<>();
- 
-        strRoles.forEach(role -> {
-          switch(role) {
-          case "admin":
-            Role adminRole = roleRepository.findByName(RoleName.ROLE_ADMIN)
-                  .orElseThrow(() -> new RuntimeException("Fail! -> Cause: User Role not find."));
-            roles.add(adminRole);
-            
-            break;
-          case "pm":
-                Role pmRole = roleRepository.findByName(RoleName.ROLE_DOCTOR)
-                  .orElseThrow(() -> new RuntimeException("Fail! -> Cause: User Role not find."));
-                roles.add(pmRole);
-                
-            break;
-          default:
-              Role userRole = roleRepository.findByName(RoleName.ROLE_USER)
-                  .orElseThrow(() -> new RuntimeException("Fail! -> Cause: User Role not find."));
-              roles.add(userRole);              
-          }
-        });
+        roles.add(userRole);   
         
         user.setRoles(roles);
         userRepository.save(user);
