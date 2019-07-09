c5c03fd5247dc62cec694d25806d6f2aec29a898
Domonkos Papp <pappdomi5@gmail.com>
2019-07-09 16:13:00 +0200

    Create role service

---
+++ src/main/java/com/example/vetapp/service/RoleService.java
@@ -0,0 +1,25 @@
+package com.example.vetapp.service;
+
+import java.util.List;
+
+import com.example.vetapp.model.Role;
+import com.example.vetapp.model.User;
+
+public class RoleService {
+
+	public User save(String userId, String roleName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Role> get(String userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void delete(String userId, String roleId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
