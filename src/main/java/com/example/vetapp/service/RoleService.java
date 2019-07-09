836b19673a7742ce11cb3412680584a7da8d50ab
Domonkos Papp <pappdomi5@gmail.com>
2019-07-09 17:02:12 +0200

    Implement save method

--- src/main/java/com/example/vetapp/service/RoleService.java
+++ src/main/java/com/example/vetapp/service/RoleService.java
@@ -1,10 +1,12 @@
 package com.example.vetapp.service;
 
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.vetapp.exception.NotFoundException;
 import com.example.vetapp.model.Role;
 import com.example.vetapp.model.User;
 import com.example.vetapp.repository.UserRepository;
@@ -18,10 +20,20 @@ public class RoleService {
 	public void setUserRepository(UserRepository userRepository) {
 		this.userRepository = userRepository;
 	}
+	
+	RoleNameService roleNameService;
+	
+	@Autowired
+	public void setRoleNameService(RoleNameService roleNameService) {
+		this.roleNameService = roleNameService;
+	}
 
 	public User save(String username, String roleName) {
-		// TODO Auto-generated method stub
-		return null;
+		User user = findUserByUsername(username);
+		Set<Role> roles = user.getRoles();
+        roles.add(roleNameService.getRoleByName(roleName));
+        user.setRoles(roles);
+		return userRepository.save(user);
 	}
 
 	public List<Role> get(String username) {
@@ -32,6 +44,13 @@ public class RoleService {
 	public void delete(String username, String roleId) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	private User findUserByUsername(String username) {
+		User user = userRepository.findByUsername(username);
+		if(user == null)
+			throw new NotFoundException("User not found!");
+		return user;
 	}
 
 }
