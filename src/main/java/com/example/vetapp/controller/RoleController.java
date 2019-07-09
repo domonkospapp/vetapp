31b41733e52cf44f7d731082e55ce85d55d912ea
Domonkos Papp <pappdomi5@gmail.com>
2019-07-09 16:13:28 +0200

    Implement role controller methods

--- src/main/java/com/example/vetapp/controller/RoleController.java
+++ src/main/java/com/example/vetapp/controller/RoleController.java
@@ -1,5 +1,8 @@
 package com.example.vetapp.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,18 +11,40 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import com.example.vetapp.model.Role;
 import com.example.vetapp.model.User;
+import com.example.vetapp.service.RoleService;
 
 @PreAuthorize("hasRole('ADMIN')")
 public class RoleController {
 	
+	@Autowired
+	RoleService roleService;
+	
     @ResponseStatus(HttpStatus.CREATED)
     @RequestMapping(value = "/{userId}/roles", method = RequestMethod.POST)
     public User addRole(
-    	@PathVariable("userId") long userId,
+    	@PathVariable("userId") String userId,
     	@RequestParam(value="roleName") String roleName
     ){
-    	return null;//roleService.save(userId, roleName);
+    	return roleService.save(userId, roleName);
+    }
+    
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(value = "/{userId}/roles", method = RequestMethod.GET)
+    public List<Role> getRoles(
+        	@PathVariable("userId") String userId
+    ){
+    	return roleService.get(userId); 
+    }
+    
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+	@RequestMapping(value = "/{userId}/roles/{roleId}", method = RequestMethod.DELETE)
+    public void deleteMember(
+    		@PathVariable("userId") String userId,
+    		@PathVariable("roleId") String roleId
+    ) {
+    	roleService.delete(userId, roleId); 
     }
 
 }
