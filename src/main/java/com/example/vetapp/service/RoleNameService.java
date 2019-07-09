b11316ce416b904a29d6525474ca6149c190cfbe
Domonkos Papp <pappdomi5@gmail.com>
2019-07-09 16:56:54 +0200

    Create RoleNameService

---
+++ src/main/java/com/example/vetapp/service/RoleNameService.java
@@ -0,0 +1,34 @@
+package com.example.vetapp.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.vetapp.exception.NotFoundException;
+import com.example.vetapp.model.Role;
+import com.example.vetapp.model.RoleName;
+import com.example.vetapp.repository.RoleRepository;
+
+public class RoleNameService {
+	
+	RoleRepository roleRepository;
+	
+	@Autowired
+	public void setRoleRepository(RoleRepository roleRepository) {
+		this.roleRepository = roleRepository;
+	}
+	
+	Role getRoleByName(String roleName) {
+		switch(roleName) {
+	        case "admin":
+	          return roleRepository.findByName(RoleName.ROLE_ADMIN)
+	                .orElseThrow(() -> new NotFoundException("Role admin not found!"));
+	        case "doctor":
+	              return roleRepository.findByName(RoleName.ROLE_DOCTOR)
+	                .orElseThrow(() -> new NotFoundException("Role doctor not found!"));
+	        case "user":
+	              return roleRepository.findByName(RoleName.ROLE_USER)
+	                .orElseThrow(() -> new NotFoundException("Role user not found!"));
+	        default:
+	            throw new NotFoundException("Role not found!");            
+		}
+	}
+}
