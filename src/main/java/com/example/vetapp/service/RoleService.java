fb046ad0a47d33358899d00dfa222e6ff5e6612d
Domonkos Papp <pappdomi5@gmail.com>
2019-07-09 16:22:11 +0200

    Change userId type to Long, Inject userService and roleRepo

--- src/main/java/com/example/vetapp/service/RoleService.java
+++ src/main/java/com/example/vetapp/service/RoleService.java
@@ -2,22 +2,41 @@ package com.example.vetapp.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.example.vetapp.model.Role;
 import com.example.vetapp.model.User;
+import com.example.vetapp.repository.RoleRepository;
 
+@Service
 public class RoleService {
+	
+	RoleRepository roleRepository;
+	
+	@Autowired
+	public void setRoleRepository(RoleRepository roleRepository) {
+		this.roleRepository = roleRepository;
+	}
+	
+	UserService userService;
+	
+	@Autowired
+	public void setUserService(UserService userService) {
+		this.userService = userService;
+	}
 
-	public User save(String userId, String roleName) {
+	public User save(Long userId, String roleName) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public List<Role> get(String userId) {
+	public List<Role> get(Long userId) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public void delete(String userId, String roleId) {
+	public void delete(Long userId, String roleId) {
 		// TODO Auto-generated method stub
 		
 	}
