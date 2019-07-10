fe08abc31a27e0759eae56931391b83303a07b27
Domonkos Papp <pappdomi5@gmail.com>
2019-07-10 21:08:22 +0200

    Create function for update profile

--- src/main/java/com/example/vetapp/service/UserService.java
+++ src/main/java/com/example/vetapp/service/UserService.java
@@ -1,8 +1,10 @@
 package com.example.vetapp.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
+import com.example.vetapp.model.User;
 import com.example.vetapp.repository.UserRepository;
 
 @Service
@@ -17,6 +19,11 @@ public class UserService {
 	
 	public boolean existByUserId(Long id){
 		return userRepository.existsById(id);
+	}
+
+	public User update(Authentication authentication, String username, String name, String email, String phone, String address) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
