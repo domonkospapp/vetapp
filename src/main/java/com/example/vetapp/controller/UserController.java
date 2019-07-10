20f1e0e53494d583ade205eb255d9c8ba37ccb85
Domonkos Papp <pappdomi5@gmail.com>
2019-07-10 21:09:54 +0200

    Create user controller with update endpoint

---
+++ src/main/java/com/example/vetapp/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.example.vetapp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.vetapp.model.User;
+import com.example.vetapp.service.UserService;
+
+@RestController
+@PreAuthorize("hasRole('USER')")
+public class UserController {
+	
+	UserService userService;
+	
+	@Autowired
+	public void setUserService(UserService userService) {
+		this.userService = userService;
+	}
+
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(value = "users/{username}", method = RequestMethod.PUT)
+    public User updateUser(
+    	Authentication authentication,
+    	@PathVariable("username") String username,
+    	@RequestParam(value="name") String name,
+    	@RequestParam(value="email") String email,
+    	@RequestParam(value="phone") String phone,
+    	@RequestParam(value="address") String address
+    ){
+    	return userService.update(authentication, username, name, email, phone, address);
+    }
+	
+
+}
