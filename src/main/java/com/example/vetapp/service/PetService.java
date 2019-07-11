8c919b2c1117f885bd4eaf67a2ab81fcf02abd3a
Domonkos Papp <pappdomi5@gmail.com>
2019-07-11 20:06:06 +0200

    Refact pet service

--- src/main/java/com/example/vetapp/service/PetService.java
+++ src/main/java/com/example/vetapp/service/PetService.java
@@ -1,16 +1,32 @@
 package com.example.vetapp.service;
 
-import org.springframework.security.core.Authentication;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.vetapp.model.Pet;
+import com.example.vetapp.model.User;
+import com.example.vetapp.repository.PetRepository;
 
 @Service
 public class PetService {
+	
+	UserService userService;
+	
+	@Autowired
+	public void setUserService(UserService userService) {
+		this.userService = userService;
+	}
+	
+	PetRepository petRepository;
+	
+	@Autowired
+	public void setPetRepository(PetRepository petRepository) {
+		this.petRepository = petRepository;
+	}
 
-	public Pet save(Authentication authentication, String username, String name, Long yearOfBirth, String type) {
-		// TODO Auto-generated method stub
-		return null;
+	public Pet save(String username, String name, Long yearOfBirth, String type) {
+		User owner = userService.findUserByUsername(username);
+		return petRepository.save(new Pet(name, yearOfBirth, type, owner));
 	}
 
 }
