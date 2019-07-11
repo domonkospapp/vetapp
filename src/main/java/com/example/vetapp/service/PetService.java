5a288bd62ed7530b94a920e4c327a1e32c6f6694
Domonkos Papp <pappdomi5@gmail.com>
2019-07-11 15:54:00 +0200

    Add auth parameter

--- src/main/java/com/example/vetapp/service/PetService.java
+++ src/main/java/com/example/vetapp/service/PetService.java
@@ -1,10 +1,12 @@
 package com.example.vetapp.service;
 
+import org.springframework.security.core.Authentication;
+
 import com.example.vetapp.model.Pet;
 
 public class PetService {
 
-	public Pet save(String username, String name, Long yearOfBirth, String type) {
+	public Pet save(Authentication authentication, String username, String name, Long yearOfBirth, String type) {
 		// TODO Auto-generated method stub
 		return null;
 	}
