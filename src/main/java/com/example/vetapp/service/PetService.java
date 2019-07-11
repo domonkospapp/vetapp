783961e16d6e0a34fe027c8752b9f071ef4de810
Domonkos Papp <pappdomi5@gmail.com>
2019-07-11 17:44:31 +0200

    Add service annotation to service

--- src/main/java/com/example/vetapp/service/PetService.java
+++ src/main/java/com/example/vetapp/service/PetService.java
@@ -1,9 +1,11 @@
 package com.example.vetapp.service;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
 
 import com.example.vetapp.model.Pet;
 
+@Service
 public class PetService {
 
 	public Pet save(Authentication authentication, String username, String name, Long yearOfBirth, String type) {
