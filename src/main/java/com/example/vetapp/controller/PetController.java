14efa45b3683f1625df407f7a5376c2678f5b3e0
Domonkos Papp <pappdomi5@gmail.com>
2019-07-11 15:39:47 +0200

    Create pet controller

---
+++ src/main/java/com/example/vetapp/controller/PetController.java
@@ -0,0 +1,34 @@
+package com.example.vetapp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.vetapp.model.Pet;
+import com.example.vetapp.service.PetService;
+
+@RestController
+public class PetController {
+	
+	@Autowired
+	PetService petService;
+	
+	@PreAuthorize("hasRole('DOCTOR') or hasRole('ADMIN')")
+    @ResponseStatus(HttpStatus.CREATED)
+    @RequestMapping(value = "users/{username}/pets", method = RequestMethod.POST)
+    public Pet addPet(
+    	@PathVariable("username") String username,
+    	@RequestParam(value="name") String name,
+    	@RequestParam(value="yearOfBirth") Long yearOfBirth,
+    	@RequestParam(value="type") String type
+    ){
+    	return petService.save(username, name, yearOfBirth, type);
+    }
+
+}
