cd5cecbd017f587662d34d17ad725e9c89504720
Domonkos Papp <pappdomi5@gmail.com>
2019-07-11 15:39:28 +0200

    Create pet service

---
+++ src/main/java/com/example/vetapp/service/PetService.java
@@ -0,0 +1,12 @@
+package com.example.vetapp.service;
+
+import com.example.vetapp.model.Pet;
+
+public class PetService {
+
+	public Pet save(String username, String name, Long yearOfBirth, String type) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
