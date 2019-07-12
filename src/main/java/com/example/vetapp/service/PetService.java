e35b9fa7c40fa2cc63fc93ebfabbf42876b26a43
Domonkos Papp <pappdomi5@gmail.com>
2019-07-12 15:12:11 +0200

    Create get function in petservice for other services

--- src/main/java/com/example/vetapp/service/PetService.java
+++ src/main/java/com/example/vetapp/service/PetService.java
@@ -3,6 +3,7 @@ package com.example.vetapp.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.vetapp.exception.NotFoundException;
 import com.example.vetapp.model.Pet;
 import com.example.vetapp.model.User;
 import com.example.vetapp.repository.PetRepository;
@@ -27,6 +28,14 @@ public class PetService {
 	public Pet save(String username, String name, Long yearOfBirth, String type) {
 		User owner = userService.findUserByUsername(username);
 		return petRepository.save(new Pet(name, yearOfBirth, type, owner));
+	}
+
+	public Pet getById(Long id) {
+		Pet pet = petRepository.findById(id).orElse(null);
+		if(pet == null) {
+			throw new NotFoundException("Pet not found!");
+		}
+		return pet;
 	}
 
 }
