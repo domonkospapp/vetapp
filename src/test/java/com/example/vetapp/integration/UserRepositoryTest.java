431bfb1326f19b26aebc0c3f6f92003996f18be1
Domonkos Papp <pappdomi5@gmail.com>
2019-07-24 16:30:35 +0200

    Test findByUsername

--- src/test/java/com/example/vetapp/integration/UserRepositoryTest.java
+++ src/test/java/com/example/vetapp/integration/UserRepositoryTest.java
@@ -1,5 +1,8 @@
 package com.example.vetapp.integration;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -7,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.example.vetapp.model.User;
 import com.example.vetapp.repository.UserRepository;
 
 @RunWith(SpringRunner.class)
@@ -19,5 +23,23 @@ public class UserRepositoryTest {
 
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Test
+	public void when_findByUsername_return_user() {
+		User user = new User("Test Test", "test", "mail@mail.com", "password");
+		entityManager.persist(user);
+		entityManager.flush();
+
+		User foundUser = userRepository.findByUsername(user.getUsername());
+		
+		assertThat(foundUser.getName()).isEqualTo(user.getName());
+	}
+	
+	@Test
+	public void when_findByUsername_not_found() {
+		User foundUser = userRepository.findByUsername("username");
+		
+		assertThat(foundUser).isEqualTo(null);
+	}
 
 }
