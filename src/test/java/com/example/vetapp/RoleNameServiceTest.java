3d4c1f723a9fb3c04f17895d1519d35a3e0c4f10
Domonkos Papp <pappdomi5@gmail.com>
2019-07-24 18:52:18 +0200

    Add test setup

--- src/test/java/com/example/vetapp/RoleNameServiceTest.java
+++ src/test/java/com/example/vetapp/RoleNameServiceTest.java
@@ -1,13 +1,19 @@
 package com.example.vetapp;
 
+import java.util.Optional;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.example.vetapp.model.Role;
+import com.example.vetapp.model.RoleName;
 import com.example.vetapp.repository.RoleRepository;
 import com.example.vetapp.service.RoleNameService;
 
@@ -29,7 +35,20 @@ public class RoleNameServiceTest {
 	@MockBean
 	private RoleRepository roleRepository;
 	
+	@Before
+	public void setUp() {
+		Optional<Role> userRole = Optional.of(new Role(RoleName.ROLE_USER));
+		Optional<Role> doctorRole = Optional.of(new Role(RoleName.ROLE_DOCTOR));
+		Optional<Role> adminRole = Optional.of(new Role(RoleName.ROLE_ADMIN));
+
+		Mockito.when(roleRepository.findByName(RoleName.ROLE_USER)).thenReturn(userRole);
+		Mockito.when(roleRepository.findByName(RoleName.ROLE_DOCTOR)).thenReturn(doctorRole);
+		Mockito.when(roleRepository.findByName(RoleName.ROLE_ADMIN)).thenReturn(adminRole);
+	}
+
 	@Test
-	public void getRoleByName() { }
+	public void getRoleByName() {
+
+	}
 
 }
