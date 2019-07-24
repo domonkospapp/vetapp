4127a4b74ec2a6adf0c5dd2a962656660cbc7be8
Domonkos Papp <pappdomi5@gmail.com>
2019-07-24 17:34:20 +0200

    Remove unused things

--- src/test/java/com/example/vetapp/integration/RoleRepositoryTest.java
+++ src/test/java/com/example/vetapp/integration/RoleRepositoryTest.java
@@ -2,13 +2,10 @@ package com.example.vetapp.integration;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.Optional;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -20,9 +17,6 @@ import com.example.vetapp.repository.RoleRepository;
 @DataJpaTest
 @TestPropertySource(locations = "classpath:application-integrationtest.properties")
 public class RoleRepositoryTest {
-	
-	@Autowired
-	private TestEntityManager entityManager;
 
 	@Autowired
 	private RoleRepository roleRepository;
