f87cdf06c3831c35aceb99b92d037d0f5cb314b3
Domonkos Papp <pappdomi5@gmail.com>
2019-08-07 14:03:01 +0200

    Setup test context

--- src/test/java/com/example/vetapp/RoleServiceTest.java
+++ src/test/java/com/example/vetapp/RoleServiceTest.java
@@ -2,23 +2,19 @@ package com.example.vetapp;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.Optional;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.example.vetapp.exception.NotFoundException;
-import com.example.vetapp.model.Role;
-import com.example.vetapp.model.RoleName;
-import com.example.vetapp.repository.RoleRepository;
+import com.example.vetapp.repository.UserRepository;
 import com.example.vetapp.service.RoleNameService;
+import com.example.vetapp.service.RoleService;
 
 @RunWith(SpringRunner.class)
 public class RoleServiceTest {
@@ -27,49 +23,35 @@ public class RoleServiceTest {
 	static class EmployeeServiceImplTestContextConfiguration {
 		
 		@Bean
-		public RoleNameService roleNameService() {
-			return new RoleNameService();
+		public RoleService roleService() {
+			return new RoleService();
 		}
 	}
 	
 	@Autowired
+	private RoleService roleService;
+	
+	@MockBean
 	private RoleNameService roleNameService;
 	
 	@MockBean
-	private RoleRepository roleRepository;
+	private UserRepository userRepository;
 	
 	@Before
 	public void setUp() {
-		Optional<Role> userRole = Optional.of(new Role(RoleName.ROLE_USER));
-		Optional<Role> doctorRole = Optional.of(new Role(RoleName.ROLE_DOCTOR));
-		Optional<Role> adminRole = Optional.of(new Role(RoleName.ROLE_ADMIN));
-
-		Mockito.when(roleRepository.findByName(RoleName.ROLE_USER)).thenReturn(userRole);
-		Mockito.when(roleRepository.findByName(RoleName.ROLE_DOCTOR)).thenReturn(doctorRole);
-		Mockito.when(roleRepository.findByName(RoleName.ROLE_ADMIN)).thenReturn(adminRole);
+//		Optional<Role> userRole = Optional.of(new Role(RoleName.ROLE_USER));
+//		Optional<Role> doctorRole = Optional.of(new Role(RoleName.ROLE_DOCTOR));
+//		Optional<Role> adminRole = Optional.of(new Role(RoleName.ROLE_ADMIN));
+//
+//		Mockito.when(roleRepository.findByName(RoleName.ROLE_USER)).thenReturn(userRole);
+//		Mockito.when(roleRepository.findByName(RoleName.ROLE_DOCTOR)).thenReturn(doctorRole);
+//		Mockito.when(roleRepository.findByName(RoleName.ROLE_ADMIN)).thenReturn(adminRole);
 	}
 
 	@Test
-	public void getRoleByName_user() {
-		Role role = roleNameService.getRoleByName("user");
-		assertThat(role.getName()).isEqualTo(RoleName.ROLE_USER);
-	}
-
-	@Test
-	public void getRoleByName_doctor() {
-		Role role = roleNameService.getRoleByName("doctor");
-		assertThat(role.getName()).isEqualTo(RoleName.ROLE_DOCTOR);
-	}
-
-	@Test
-	public void getRoleByName_admin() {
-		Role role = roleNameService.getRoleByName("admin");
-		assertThat(role.getName()).isEqualTo(RoleName.ROLE_ADMIN);
-	}
-
-	@Test(expected = NotFoundException.class)
-	public void getRoleByName_with_not_valid_role() {
-		roleNameService.getRoleByName("adminn");
+	public void save_role() {
+		//Role role = new Role(RoleName.ROLE_USER);
+		//assertThat(role.getName()).isEqualTo(RoleName.ROLE_USER);
 	}
 
 }
