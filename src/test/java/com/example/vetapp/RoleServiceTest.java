592acd6a31d6aee55a064fb6234793c3a50e099c
Domonkos Papp <pappdomi5@gmail.com>
2019-08-07 14:26:13 +0200

    Implement save role test

--- src/test/java/com/example/vetapp/RoleServiceTest.java
+++ src/test/java/com/example/vetapp/RoleServiceTest.java
@@ -6,12 +6,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.example.vetapp.model.Role;
+import com.example.vetapp.model.RoleName;
+import com.example.vetapp.model.User;
 import com.example.vetapp.repository.UserRepository;
 import com.example.vetapp.service.RoleNameService;
 import com.example.vetapp.service.RoleService;
@@ -36,22 +40,33 @@ public class RoleServiceTest {
 	
 	@MockBean
 	private UserRepository userRepository;
+
+	private Role userRole;
+	private Role doctorRole;
+	private Role adminRole;
 	
 	@Before
 	public void setUp() {
-//		Optional<Role> userRole = Optional.of(new Role(RoleName.ROLE_USER));
-//		Optional<Role> doctorRole = Optional.of(new Role(RoleName.ROLE_DOCTOR));
-//		Optional<Role> adminRole = Optional.of(new Role(RoleName.ROLE_ADMIN));
-//
-//		Mockito.when(roleRepository.findByName(RoleName.ROLE_USER)).thenReturn(userRole);
-//		Mockito.when(roleRepository.findByName(RoleName.ROLE_DOCTOR)).thenReturn(doctorRole);
-//		Mockito.when(roleRepository.findByName(RoleName.ROLE_ADMIN)).thenReturn(adminRole);
+		userRole = new Role(RoleName.ROLE_USER);
+		doctorRole = new Role(RoleName.ROLE_DOCTOR);
+		adminRole = new Role(RoleName.ROLE_ADMIN);
+
+		Mockito.when(roleNameService.getRoleByName("user")).thenReturn(userRole);
+		Mockito.when(roleNameService.getRoleByName("doctor")).thenReturn(doctorRole);
+		Mockito.when(roleNameService.getRoleByName("admin")).thenReturn(adminRole);
 	}
 
 	@Test
 	public void save_role() {
-		//Role role = new Role(RoleName.ROLE_USER);
-		//assertThat(role.getName()).isEqualTo(RoleName.ROLE_USER);
+		User user = new User("Test User", "test_user", "test@test.com", "password");
+
+		Mockito.when(userRepository.findByUsername(user.getUsername())).thenReturn(user);
+		Mockito.when(userRepository.save(user)).thenReturn(user);
+		
+		User userWithRole = roleService.save(user.getUsername(), "user");
+
+		assertThat(userWithRole.getRoles().size()).isEqualTo(1);
+		assertThat(userWithRole.getRoles().contains(userRole)).isEqualTo(true);
 	}
 
 }
